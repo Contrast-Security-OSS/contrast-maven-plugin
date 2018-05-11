e7358ed9362c187c49ae0798972d5ee47a44e7fe
Gary Moore <gary.moore@contrastsecurity.com>
2018-05-11 14:11:01 -0400

    added tests around build number juggling

--- src/test/java/com/contrastsecurity/maven/plugin/InstallAgentContrastMavenMojoTest.java
+++ src/test/java/com/contrastsecurity/maven/plugin/InstallAgentContrastMavenMojoTest.java
@@ -3,13 +3,18 @@ package com.contrastsecurity.maven.plugin;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import static org.junit.Assert.*;
 
 public class InstallAgentContrastMavenMojoTest {
     InstallAgentContrastMavenMojo installMojo;
     Date now;
+
+    @Rule
+    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
     @Before
     public void setUp() {
@@ -19,6 +24,7 @@ public class InstallAgentContrastMavenMojoTest {
         installMojo.contrastAgentLocation = "/usr/local/bin/contrast.jar";
 
         now = new Date();
+        environmentVariables.clear("TRAVIS_BUILD_NUMBER", "CIRCLE_BUILD_NUM");
     }
 
    @Test
@@ -35,6 +41,24 @@ public class InstallAgentContrastMavenMojoTest {
         String expectedVersion = new SimpleDateFormat("yyyyMMddHHmmss").format(now);
         assertEquals("caddyshack-" + expectedVersion,installMojo.computeAppVersion(now));
         assertEquals("caddyshack-" + expectedVersion,installMojo.computeAppVersion(now));
+    }
+
+    @Test
+    public void testGenerateAppVersionTravis() {
+        installMojo.userSpecifiedAppVersion = null;
+        installMojo.computedAppVersion = null;
+        environmentVariables.set("TRAVIS_BUILD_NUMBER", "19");
+        assertEquals("caddyshack-19",installMojo.computeAppVersion(now));
+        assertEquals("caddyshack-19",installMojo.computeAppVersion(now));
+    }
+
+    @Test
+    public void testGenerateAppVersionCircle() {
+        installMojo.userSpecifiedAppVersion = null;
+        installMojo.computedAppVersion = null;
+        environmentVariables.set("TRAVIS_BUILD_NUMBER", "circle");
+        assertEquals("caddyshack-circle",installMojo.computeAppVersion(now));
+        assertEquals("caddyshack-circle",installMojo.computeAppVersion(now));
     }
 
     @Test
