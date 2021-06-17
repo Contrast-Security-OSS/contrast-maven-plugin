af232bce46e07835a571db177e51c63b97f7bddf
Johnathan Gilday <johnathan.gilday@contrastsecurity.com>
2021-06-17 10:52:05 -0400

    🚧 JAVA-3138 Clean-up for review

--- src/test/java/com/contrastsecurity/maven/plugin/it/InstallAgentContrastAgentMojoIT.java
+++ src/test/java/com/contrastsecurity/maven/plugin/it/InstallAgentContrastAgentMojoIT.java
@@ -11,6 +11,25 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Functional test for the "install" goal.
+ *
+ * <p>Verifies that the agent downloads a Contrast Java agent from the Contrast API. This test lacks
+ * the following verifications for this goal:
+ *
+ * <ul>
+ *   <li>Does not verify that the plugin configures the maven-surefire-plugin's argLine property to
+ *       include the agent
+ *   <li>Does not verify that the plugin configures the spring-boot-maven-plugin's
+ *       "run.jvmArguments" property to include the agent
+ *   <li>Does not verify that the agent returned by the Contrast API is preconfigured to report to
+ *       the user's Contrast organization
+ * </ul>
+ *
+ * We accept the risk that the aforementioned features are not verified in this test, because we
+ * plan to make breaking changes to this goal imminently. This functional test serves mainly as a
+ * guiding example for future functional tests.
+ */
 final class InstallAgentContrastAgentMojoIT {
 
   @ContrastAPIStub
@@ -32,8 +51,10 @@ final class InstallAgentContrastAgentMojoIT {
     final Properties connectionProperties = contrast.connection().toProperties();
     verifier.setSystemProperties(connectionProperties);
 
+    // WHEN execute the "verify" goal
     verifier.executeGoal("verify");
 
+    // THEN the plugin retrieves a Contrast Java agent from the Contrast API without errors
     verifier.verifyErrorFreeLog();
     verifier.assertFilePresent("target/contrast.jar");
   }
