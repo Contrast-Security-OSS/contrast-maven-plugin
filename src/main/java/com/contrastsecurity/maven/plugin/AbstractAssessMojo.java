126c52a4f6695d587bf01c05d428c83fab0294e6
Johnathan Gilday <johnathan.gilday@contrastsecurity.com>
2021-08-19 14:01:53 -0400

    🔥 JAVA-3298 Remove Utilities from Transitive Dependencies

--- src/main/java/com/contrastsecurity/maven/plugin/AbstractAssessMojo.java
+++ src/main/java/com/contrastsecurity/maven/plugin/AbstractAssessMojo.java
@@ -1,6 +1,5 @@
 package com.contrastsecurity.maven.plugin;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -44,8 +43,8 @@ abstract class AbstractAssessMojo extends AbstractContrastMojo {
   @Parameter(property = "serverName", required = true)
   private String serverName;
 
-  void verifyAppIdOrNameNotBlank() throws MojoExecutionException {
-    if (StringUtils.isBlank(appId) && StringUtils.isBlank(appName)) {
+  void verifyAppIdOrNameNotNull() throws MojoExecutionException {
+    if (appId == null && appName == null) {
       throw new MojoExecutionException(
           "Please specify appId or appName in the plugin configuration.");
     }
